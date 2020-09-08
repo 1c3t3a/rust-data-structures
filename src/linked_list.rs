3052119b198dfded294e1f3630c5bae186741478
Colin Mikolajczak <mikolajc@hm.edu>
2020-09-08 09:43:19 +0200

    fixed requested changes

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -76,7 +76,6 @@ where
         }
         result
     }
-    
 
     /// Appends a new element to the list.
     /// # Example
@@ -149,103 +148,107 @@ where
 
     pub fn is_sorted(&self) -> bool {
         match &self.head {
-            Some(head) => return head.is_sorted(),
+            Some(head) => head.is_sorted(),
             None => false,
         }
     }
 
-    
-/// Why Merge-Sort?
-///
-/// Quick sort works well for sorting in-place. 
-/// In particular, most of the operations can be defined in terms of swapping pairs of elements 
-/// in an array. To do that, however, you normally "walk" through the array with two pointers 
-/// (or indexes, etc.) One starts at the beginning of the array and the other at the end. 
-/// Both then work their way toward the middle (and you're done with a particular partition step 
-/// when they meet). That's expensive with files, because files are oriented primarily toward reading
-/// in one direction, from beginning to end. Starting from the end and seeking backwards is usually
-/// relatively expensive.
+    /// Why Merge-Sort?
+    ///
+    /// Quick sort works well for sorting in-place.
+    /// In particular, most of the operations can be defined in terms of swapping pairs of elements
+    /// in an array. To do that, however, you normally "walk" through the array with two pointers
+    /// (or indexes, etc.) One starts at the beginning of the array and the other at the end.
+    /// Both then work their way toward the middle (and you're done with a particular partition step
+    /// when they meet). That's expensive with files, because files are oriented primarily toward reading
+    /// in one direction, from beginning to end. Starting from the end and seeking backwards is usually
+    /// relatively expensive.
 
-/// At least in its simplest incarnation, merge sort is pretty much the opposite. 
-/// The easy way to implement it only requires looking through the data in one direction, 
-/// but involves breaking the data into two separate pieces, sorting the pieces, 
-/// then merging them back together.
+    /// At least in its simplest incarnation, merge sort is pretty much the opposite.
+    /// The easy way to implement it only requires looking through the data in one direction,
+    /// but involves breaking the data into two separate pieces, sorting the pieces,
+    /// then merging them back together.
 
-/// With a linked list, it's easy to take (for example) alternating elements in one linked list, 
-/// and manipulate the links to create two linked lists from those same elements instead. 
-/// With an array, rearranging elements so alternating elements go into separate arrays is easy
-/// if you're willing to create a copy as big as the original data, but otherwise rather more
-/// non-trivial.
+    /// With a linked list, it's easy to take (for example) alternating elements in one linked list,
+    /// and manipulate the links to create two linked lists from those same elements instead.
+    /// With an array, rearranging elements so alternating elements go into separate arrays is easy
+    /// if you're willing to create a copy as big as the original data, but otherwise rather more
+    /// non-trivial.
 
-/// Likewise, merging with arrays is easy if you merge elements from the source arrays
-/// into a new array with the data in order -- but to do it in place without creating a whole
-/// new copy of the data is a whole different story. With a linked list, merging elements together
-/// from two source lists into a single target list is trivial -- again, you just manipulate links,
-/// without copying elements.
+    /// Likewise, merging with arrays is easy if you merge elements from the source arrays
+    /// into a new array with the data in order -- but to do it in place without creating a whole
+    /// new copy of the data is a whole different story. With a linked list, merging elements together
+    /// from two source lists into a single target list is trivial -- again, you just manipulate links,
+    /// without copying elements.
 
-/// As for using Quicksort to produce the sorted runs for an external merge sort, 
-/// it does work, but it's (decidedly) sub-optimal as a rule. To optimize a merge-sort, 
-/// you normally want to maximize the lengths of each sorted "run" as you produce it. 
-/// If you simply read in the data that will fit in memory, Quicksort it and write it out, 
-/// each run will be restricted to (a little less than) the size of the available memory.
+    /// As for using Quicksort to produce the sorted runs for an external merge sort,
+    /// it does work, but it's (decidedly) sub-optimal as a rule. To optimize a merge-sort,
+    /// you normally want to maximize the lengths of each sorted "run" as you produce it.
+    /// If you simply read in the data that will fit in memory, Quicksort it and write it out,
+    /// each run will be restricted to (a little less than) the size of the available memory.
 
-/// You can do quite a bit better than that as a rule though. 
-/// You start by reading in a block of data, but instead of using a Quicksort on it, you build a heap. 
-/// Then, as you write each item out from the heap into the sorted "run" file, you read another item 
-/// in from your input file. If it's larger than the item you just wrote to disk, you insert it into 
-/// your existing heap, and repeat.
+    /// You can do quite a bit better than that as a rule though.
+    /// You start by reading in a block of data, but instead of using a Quicksort on it, you build a heap.
+    /// Then, as you write each item out from the heap into the sorted "run" file, you read another item
+    /// in from your input file. If it's larger than the item you just wrote to disk, you insert it into
+    /// your existing heap, and repeat.
 
-/// Items that are smaller (i.e., belong before items that have already been written) you keep 
-/// separate, and build into a second heap. When (and only when) your first heap is empty, 
-/// and the second heap has taken over all the memory, you quit writing items to the existing "run" file, 
-/// and start on a new one.
+    /// Items that are smaller (i.e., belong before items that have already been written) you keep
+    /// separate, and build into a second heap. When (and only when) your first heap is empty,
+    /// and the second heap has taken over all the memory, you quit writing items to the existing "run" file,
+    /// and start on a new one.
 
-/// Exactly how effective this will be depends on the initial order of the data. 
-/// In the worst case (input sorted in inverse order) it does no good at all. In the best case 
-/// (input already sorted) it lets you "sort" the data in a single run through the input. 
-/// In an average case (input in random order) it lets you approximately double the length of 
-/// each sorted run, which will typically improve speed by around 20-25% 
-/// (though the percentage varies depending on how much larger your data is than the available memory).
+    /// Exactly how effective this will be depends on the initial order of the data.
+    /// In the worst case (input sorted in inverse order) it does no good at all. In the best case
+    /// (input already sorted) it lets you "sort" the data in a single run through the input.
+    /// In an average case (input in random order) it lets you approximately double the length of
+    /// each sorted run, which will typically improve speed by around 20-25%
+    /// (though the percentage varies depending on how much larger your data is than the available memory).
 
     pub fn sort(&mut self) {
-        if self.head == None {
+        if self.head.is_none() {
             return;
         }
 
         let (mut front, mut back) = self.split();
 
-        if front.len > 1  {
+        if front.len > 1 {
             front.sort();
         }
 
         if back.len > 1 {
             back.sort();
         }
-    
-        self.head = Some(Box::new(Node::from(LinkedList::merge(&mut front, &mut back))));
+
+        self.head = Some(Box::new(Node::from(LinkedList::merge(
+            &mut front, &mut back,
+        ))));
     }
 
     fn split(&mut self) -> (LinkedList<T>, LinkedList<T>) {
-        let back = self.head.as_mut().unwrap().get_back(self.len / 2, 0).unwrap();
+        let back = self
+            .head
+            .as_mut()
+            .unwrap()
+            .get_back(self.len / 2, 0)
+            .unwrap();
         let front = self.head.take().unwrap();
 
         (LinkedList::from(*front), LinkedList::from(*back))
-    }    
+    }
 
     fn merge(front: &mut LinkedList<T>, back: &mut LinkedList<T>) -> Self {
         let mut result: Node<T>;
-        
+
         if front.head.is_none() {
             return LinkedList::from(*back.head.take().unwrap());
-        }
-        else if back.head.is_none() {
+        } else if back.head.is_none() {
             return LinkedList::from(*front.head.take().unwrap());
         }
 
         if front.head.as_ref().unwrap().value <= back.head.as_ref().unwrap().value {
             result = Node::new(front.pop_front().unwrap());
-        }
-        else {
+        } else {
             result = Node::new(back.pop_front().unwrap());
         }
 
@@ -345,11 +348,10 @@ where
         self.next = Some(Box::new(new));
     }
 
-    fn get_back(&mut self, index: usize, mut cur: usize) -> Option<Box<Node<T>>>{
+    fn get_back(&mut self, index: usize, mut cur: usize) -> Option<Box<Node<T>>> {
         if cur + 1 == index {
             self.next.take()
-        }
-        else {
+        } else {
             cur += 1;
             self.next.as_mut().unwrap().get_back(index, cur)
         }
@@ -360,17 +362,19 @@ where
         match &look.next {
             Some(val) => {
                 if look.value <= val.value {
-                    return val.is_sorted()
-                }
-                else {
+                    return val.is_sorted();
+                } else {
                     return false;
                 }
-            },
-            None => return true,
+            }
+            None => true,
         }
     }
 
-    fn new(value: T) -> Self where T: Ord{
+    fn new(value: T) -> Self
+    where
+        T: Ord,
+    {
         Node { next: None, value }
     }
 
@@ -450,8 +454,11 @@ macro_rules! list {
     }};
 }
 
-impl<T> From<Vec<T>> for LinkedList<T> where T: Eq + Ord{
-    fn from (list: Vec<T>) -> Self {
+impl<T> From<Vec<T>> for LinkedList<T>
+where
+    T: Eq + Ord,
+{
+    fn from(list: Vec<T>) -> Self {
         let mut result = list![];
         for elem in list {
             result.insert(elem);
@@ -460,16 +467,25 @@ impl<T> From<Vec<T>> for LinkedList<T> where T: Eq + Ord{
     }
 }
 
-impl<T> From<LinkedList<T>> for Node<T> where T: Eq + Ord {
+impl<T> From<LinkedList<T>> for Node<T>
+where
+    T: Eq + Ord,
+{
     fn from(list: LinkedList<T>) -> Self {
         *list.head.unwrap()
     }
 }
 
-impl<T> From<Node<T>> for LinkedList<T> where T: Eq + Ord {
+impl<T> From<Node<T>> for LinkedList<T>
+where
+    T: Eq + Ord,
+{
     fn from(node: Node<T>) -> Self {
         let length = node.get_length();
-        LinkedList { head: Some(Box::new(node)), len: length }
+        LinkedList {
+            head: Some(Box::new(node)),
+            len: length,
+        }
     }
 }
 
@@ -577,7 +593,6 @@ mod test {
 
     #[test]
     fn test_is_sorted() {
-
         let mut sut = list![];
         sut.insert(4);
         sut.insert(3);
@@ -592,7 +607,6 @@ mod test {
 
     #[test]
     fn test_remove_head() {
-
         let mut sut: LinkedList<u32> = list![];
         sut.insert(45);
         sut.insert(56);
@@ -609,12 +623,11 @@ mod test {
         assert_eq!(val, 56);
         assert_eq!(sut.len, 5);
         println!("{}", val);
-
     }
 
     #[test]
     fn test_get_length() {
-        let mut sut: LinkedList<u32> = list![1,2,4,5,6];
+        let mut sut: LinkedList<u32> = list![1, 2, 4, 5, 6];
 
         let head = sut.head.take().unwrap();
 
@@ -662,7 +675,7 @@ mod test {
 
     #[test]
     fn test_sort() {
-        let mut sut = list![5,4,3,2,1];
+        let mut sut = list![5, 4, 3, 2, 1];
         assert!(!sut.is_sorted());
         sut.sort();
         assert!(sut.is_sorted());
@@ -677,7 +690,7 @@ mod test {
 
     #[test]
     fn test_sort_advanced() {
-        let mut sut = list![1938,234,239842,28,32,2,4,2382,1093482,23,34,89,2];
+        let mut sut = list![1938, 234, 239842, 28, 32, 2, 4, 2382, 1093482, 23, 34, 89, 2];
         assert!(!sut.is_sorted());
         sut.sort();
         assert!(sut.is_sorted());
