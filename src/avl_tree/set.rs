b1633a04ad7239a016de0a6e18d043a9b7c0063c
Colin Mikolajczak <mikolajc@hm.edu>
2020-10-22 21:23:29 +0200

    implemented avl tree

--- src/avl_tree/set.rs
+++ src/avl_tree/set.rs
@@ -1,5 +1,6 @@
 use crate::avl_tree::tree::*;
 use std::{cmp::Ordering, iter::FromIterator};
+use std::mem::replace;
 
 #[derive(Debug, PartialEq, Clone)]
 struct AVLTreeSet<T: Ord> {
@@ -45,7 +46,178 @@ impl<'a, T: 'a + Ord> AVLTreeSet<T> {
         }
 
         true
-    }   
+    }
+    
+    pub fn take(&mut self, value: &T) -> Option<T> {
+        let mut prev_ptrs = Vec::<*mut AVLNode<T>>::new();
+        let mut current_tree = &mut self.root;
+        let mut target_value = None;
+
+        while let Some(current_node) = current_tree {
+            match current_node.value.cmp(&value) {
+                Ordering::Less => {
+                    prev_ptrs.push(&mut **current_node);
+                    current_tree = &mut current_node.right;
+                }
+                Ordering::Equal => {
+                    target_value = Some(&mut **current_node);
+                    break;
+                }
+                Ordering::Greater => {
+                    prev_ptrs.push(&mut **current_node);
+                    current_tree = &mut current_node.left;
+                }
+            }
+        }
+
+        // Target does not exist in tree
+        if target_value.is_none() {
+            return None;
+        }
+
+        let target_node = target_value.unwrap();
+
+        // 3 Cases: No children, left child, right child
+        let taken = if target_node.left.is_none() || target_node.right.is_none() {
+            // Left child
+            if let Some(left_node) = target_node.left.take() {
+                replace(target_node, *left_node).value
+            }
+            // Right child
+            else if let Some(right_node) = target_node.right.take() {
+                replace(target_node, *right_node).value
+            }
+            // No children
+            else if let Some(prev_ptr) = prev_ptrs.pop() {
+                let prev_node = unsafe { &mut *prev_ptr };
+
+                // Determine which node to take
+                let inner_value = if let Some(left_node) = prev_node.left.as_ref() {
+                    // Target was left node from parent
+                    if left_node.value == target_node.value {
+                        prev_node.left.take().unwrap().value
+                    }
+                    // Target was right node from parent
+                    else {
+                        prev_node.right.take().unwrap().value
+                    }
+                }
+                // Target was right all along, since left is None
+                else {
+                    prev_node.right.take().unwrap().value
+                };
+
+                prev_node.update_height();
+                prev_node.rebalance();
+
+                inner_value
+            }
+            // Fourth boring case: Tree is root
+            else {
+                self.root.take().unwrap().value
+            }
+        }
+        // Find Inorder-Successor
+        else {
+            AVLTreeSet::find_inorder_succesor(target_node)
+        };
+
+        // Update for every touched Node
+        for node_ptr in prev_ptrs.into_iter().rev() {
+            let node = unsafe { &mut *node_ptr };
+            node.update_height();
+            node.rebalance();
+        }
+
+        Some(taken)
+    }
+
+     fn find_inorder_succesor(target_node: &mut AVLNode<T>) -> T {
+        let right_tree = &mut target_node.right;
+            
+        // Left tree of right is None, take first right
+        if right_tree.as_ref().unwrap().left.is_none() {
+            let mut right_node = right_tree.take().unwrap();
+
+            let inner_value = replace(&mut target_node.value, right_node.value);
+            replace(&mut target_node.right, right_node.right.take());
+
+            target_node.update_height();
+            target_node.rebalance();
+
+            inner_value
+        }
+        // Take leftest(^^) left node
+        else {
+            let mut next_tree = right_tree;
+            let mut left_ptrs = Vec::<*mut AVLNode<T>>::new();
+
+            // iterate to leftest
+            while let Some(next_left_node) = next_tree {
+                if next_left_node.left.is_some() {
+                    left_ptrs.push(&mut **next_left_node);
+                }
+                next_tree = &mut next_left_node.left;
+            }
+
+            let parent_leftest_node = unsafe { &mut *left_ptrs.pop().unwrap() };
+
+            let mut leftest_node = parent_leftest_node.left.take().unwrap();
+
+            // Taken node is now filled with leftest value
+            let inner_value = replace(&mut target_node.value, leftest_node.value);
+
+            // Leftest node is now the right child of former leftest, 
+            // because leftest has no left child and if right child is none, then thats the end of this tree
+            replace(&mut parent_leftest_node.left, leftest_node.right.take());
+
+            // Start at the bottom with updating
+            parent_leftest_node.update_height();
+            parent_leftest_node.rebalance();
+
+            // Up to the children of target
+            // Rev because into iter starts at the first inserted item, we need the last inserted first
+            for node_ptr in left_ptrs.into_iter().rev() {
+                let node = unsafe { &mut *node_ptr };
+                node.update_height();
+                node.rebalance();
+            }
+
+            // At last of course target node to update
+            target_node.update_height();
+            target_node.rebalance();
+
+            inner_value
+        }
+     }
+
+    pub fn remove(&mut self, value: &T) -> bool {
+        self.take(value).is_some()
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        self.get(value).is_some()
+    }
+    
+    pub fn get(&self, value: &T) -> Option<&T> {
+        let mut current_tree =  &self.root;
+    
+        while let Some(current_node) = current_tree {
+            match current_node.value.cmp(&value) {
+                Ordering::Less => {
+                    current_tree = &current_node.right;
+                }
+                Ordering::Equal => {
+                    return Some(&current_node.as_ref().value);
+                }
+                Ordering::Greater => {
+                    current_tree = &current_node.left;
+                }
+            }
+        }
+        None
+    }
+
 }
 
 impl<T: Ord> FromIterator<T> for AVLTreeSet<T> {
